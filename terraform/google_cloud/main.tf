5696d4d0f9a698d26e4b5bb4cb95f6686dd4fb43
yemiwebby <yemiwebby@gmail.com>
2025-05-23 12:16:19 +0100

    udpae

--- terraform/google_cloud/main.tf
+++ terraform/google_cloud/main.tf
@@ -46,6 +46,18 @@ resource "google_compute_firewall" "http-5000" {
   source_ranges = ["0.0.0.0/0"]
 }
 
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = data.google_compute_network.default.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
 resource "google_compute_instance" "default" {
   name = "default"
   machine_type = "e2-small"
